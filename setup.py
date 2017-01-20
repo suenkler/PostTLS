9fa978c6673759142a875d9b05a4f9c110f13718
Gerrit <gerrit1986@gmail.com>
2017-01-20 23:31:50 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-posttls',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='AGPLv3',
+    description='Postfix' Transport Encryption under Control of the User',
+    long_description=README.md,
+    url='https://posttls.com/',
+    author='Hendrik Suenkler',
+    author_email='hendrik@posttls.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: AGPLv3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
